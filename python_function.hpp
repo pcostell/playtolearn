3db084f0ab05137eded07bfed87d7c3ce94b36dc
Patrick Costello <patrickc@cs.stanford.edu>
2011-10-07 16:13:10 -0700

    Added comments to PythonFunction

--- python_function.hpp
+++ python_function.hpp
@@ -9,6 +9,10 @@
 
 namespace EduGame {
 
+/**
+ * Allows for the execution of a python function from c++. Passes a map
+ * from string->string for passing data between the two languages.
+ **/
 class PythonFunction {
 public:
 
@@ -17,13 +21,26 @@ public:
     explicit PythonExecutionError (const std::string& what_arg);
   };
 
+  /**
+   * Constructor
+   * Args:
+   * pythonCode: the python code to execute.
+   */
 	PythonFunction(const std::string & pythonCode);
 	
-	void execute(std::string functionName, std::map<std::string, std::string> & state) const;
+  /**
+   * Execute the python function.
+   * Args:
+   * functionName: The name of the python function to call.
+   * state: The map to pass to the python function as a python dict.
+   */
+	void execute(std::string functionName, 
+               std::map<std::string, std::string> & state) const;
 
 private:
 
-  void extractMapFromDict(std::map<std::string, std::string> & cppMap, const boost::python::dict & pythonDict) const;
+  void extractMapFromDict(std::map<std::string, std::string> & cppMap, 
+                          const boost::python::dict & pythonDict) const;
   void handlePythonError() const;
 
 	boost::python::object mainModule;
